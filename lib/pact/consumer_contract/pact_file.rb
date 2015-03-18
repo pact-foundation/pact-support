409bde5e4dc81a41b50668e725bbb69dba55382e
lifei zhou <lifeiz@gmail.com>
2015-03-18 21:49:29 +1100

    support url including basic authentication info, e.g.: http://username:password@packtbroker.com

--- lib/pact/consumer_contract/pact_file.rb
+++ lib/pact/consumer_contract/pact_file.rb
@@ -5,10 +5,10 @@ module Pact
     extend self
 
     def read uri, options = {}
-      open_options = options[:username] ? {http_basic_authentication:[options[:username],options[:password]]} : {}
-      pact = open(uri.to_s, open_options) { | file | file.read }
+      uri_string = uri.to_s
+      pact = render_pact(uri_string, options)
       if options[:save_pactfile_to_tmp]
-        save_pactfile_to_tmp pact, ::File.basename(uri.to_s)
+        save_pactfile_to_tmp pact, ::File.basename(uri_string)
       end
       pact
     rescue StandardError => e
@@ -20,6 +20,18 @@ module Pact
     def save_pactfile_to_tmp pact, name
       ::FileUtils.mkdir_p Pact.configuration.tmp_dir
       ::File.open(Pact.configuration.tmp_dir + "/#{name}", "w") { |file|  file << pact}
+    end
+
+    def render_pact uri_string, options
+      uri_obj = URI(uri_string)
+      uri_user_info = uri_obj.userinfo
+      if(uri_user_info)
+        options[:username] = uri_obj.user unless options[:username]
+        options[:password] = uri_obj.password unless options[:password]
+        uri_string = uri_string.sub("#{uri_user_info}@", '')
+      end
+      open_options = options[:username] ? {http_basic_authentication:[options[:username],options[:password]]} : {}
+      open(uri_string, open_options) { | file | file.read }
     end
   end
 end
