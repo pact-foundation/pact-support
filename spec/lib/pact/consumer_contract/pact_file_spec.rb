61ceda108282f2d6849dd424d9d6aca543329579
Taiki Ono <taiks.4559@gmail.com>
2016-04-28 15:50:29 +0900

    Re-write test with WebMock

--- spec/lib/pact/consumer_contract/pact_file_spec.rb
+++ spec/lib/pact/consumer_contract/pact_file_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'pact/consumer_contract/pact_file'
+require 'base64' # XXX: https://github.com/bblimke/webmock/pull/611
 
 module Pact
   describe PactFile do
@@ -7,8 +8,11 @@ module Pact
       let(:uri_without_userinfo) { 'http://pactbroker.com'}
       let(:pact_content) { 'api contract'}
       context 'without basic authentication' do
+        before do
+          stub_request(:get, uri_without_userinfo).to_return(body: pact_content)
+        end
+
         it 'should open uri to get pact file content' do
-          expect(PactFile).to receive(:open).with(uri_without_userinfo, {}).and_return(pact_content)
           expect(PactFile.render_pact(uri_without_userinfo, {})).to eq(pact_content)
         end
       end
@@ -19,25 +23,26 @@ module Pact
         let(:options) do
           { username: username, password: password }
         end
+        before do
+          stub_request(:get, uri_without_userinfo).with(basic_auth: [username, password]).to_return(body: pact_content)
+        end
+
         context 'when userinfo is specified in the option' do
           it 'should open uri to get pact file content with userinfo in the options' do
-            expect(PactFile).to receive(:open).with(uri_without_userinfo, {http_basic_authentication:[username, password]})
-                                .and_return(pact_content)
             expect(PactFile.render_pact(uri_without_userinfo, options)).to eq(pact_content)
           end
+
           let(:uri_with_userinfo) { 'http://dummyuser:dummyps@pactbroker.com'}
+
           it 'should use userinfo in options which overwrites userinfo in url' do
-            expect(PactFile).to receive(:open).with(uri_without_userinfo, {http_basic_authentication:[username, password]})
-                                .and_return(pact_content)
             expect(PactFile.render_pact(uri_with_userinfo, options)).to eq(pact_content)
           end
         end
 
         context 'when user info is specified in url' do
           let(:uri_with_userinfo) { "http://#{username}:#{password}@pactbroker.com"}
+
           it 'should open uri to get pact file content with userinfo in the uri' do
-            expect(PactFile).to receive(:open).with(uri_without_userinfo, {http_basic_authentication:[username, password]})
-                                .and_return(pact_content)
             expect(PactFile.render_pact(uri_with_userinfo, {})).to eq(pact_content)
           end
         end
