50ea21fbdbc55e12a72b0f6c0a558f84208c4c2c
Beth Skurrie <bethesque@users.noreply.github.com>
2016-06-09 16:01:34 +1000

    Update json_differ.rb

--- lib/pact/shared/json_differ.rb
+++ lib/pact/shared/json_differ.rb
@@ -4,8 +4,8 @@ module Pact
   class JsonDiffer
 
     extend Matchers
-
-
+    
+    # Delegates to https://github.com/bethesque/pact-support/blob/master/lib/pact/matchers/matchers.rb#L25
     def self.call expected, actual, options = {}
       diff expected, actual, options
     end
