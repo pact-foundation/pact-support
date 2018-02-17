372812679c45687a9d78a4ca12e7825795f19723
Jay Hayes <ur@iamvery.com>
2018-02-17 08:12:41 -0600

    Fix redefined method warning
    Best I can tell, this method was men to be defined outside of the
    initializer, but `ends` go away from the author.

--- lib/pact/matching_rules/jsonpath.rb
+++ lib/pact/matching_rules/jsonpath.rb
@@ -48,10 +48,10 @@ module Pact
             @path.last << token
           end
         end
+      end
 
-        def to_s
-          path.join
-        end
+      def to_s
+        path.join
       end
     end
   end
