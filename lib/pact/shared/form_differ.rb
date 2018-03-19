pact-foundation/pact-support

commit 3d2a48829dcd81fbb66e0d2b2562dc7abe9be94f

Fix a bunch of circular reference warnings
Honestly I'm not tremendously confident with this change. One problem is
that the files no longer require their "matcher" feature, so if they're
loaded directly (instead of as a side-effect of loading pact/matchers)
then you may see errors. 🤔

lib/pact/shared/form_differ.rb
@@ -1,4 +1,3 @@
-require 'pact/matchers/matchers'
 require 'uri'
 
 module Pact

lib/pact/shared/json_differ.rb
@@ -1,5 +1,3 @@
-require 'pact/matchers/matchers'
-
 module Pact
   class JsonDiffer
 

lib/pact/shared/text_differ.rb
@@ -1,4 +1,3 @@
-require 'pact/matchers/matchers'
 require 'pact/matchers/difference'
 
 module Pact

lib/pact/consumer_contract/query_string.rb
@@ -1,5 +1,4 @@
 require 'pact/shared/active_support_support'
-require 'pact/matchers'
 
 module Pact
   class QueryString

lib/pact/shared/request.rb
@@ -1,4 +1,3 @@
-require 'pact/matchers'
 require 'pact/symbolize_keys'
 require 'pact/consumer_contract/headers'
 require 'pact/consumer_contract/query'

commit d80e0fffabf8db5c65ae82e3cf4eeb08ea63e4ab

fix: dynamically load pact/matchers
This fixes the require issues caused by 3d2a48829dcd81fbb66e0d2b2562dc7abe9be94f

lib/pact/shared/form_differ.rb
@@ -3,10 +3,9 @@ require 'uri'
 module Pact
   class FormDiffer
 
-    extend Matchers
-
     def self.call expected, actual, options = {}
-      diff to_hash(expected), to_hash(actual), options
+      require 'pact/matchers' # avoid recursive loop between this file and pact/matchers
+      ::Pact::Matchers.diff to_hash(expected), to_hash(actual), options
     end
 
     def self.to_hash form_body
@@ -29,6 +28,5 @@ module Pact
         hash[key] << value
       end
     end
-
   end
 end

lib/pact/shared/json_differ.rb
@@ -1,13 +1,10 @@
 module Pact
   class JsonDiffer
 
-    extend Matchers
-    
     # Delegates to https://github.com/pact-foundation/pact-support/blob/master/lib/pact/matchers/matchers.rb#L25
     def self.call expected, actual, options = {}
-      diff expected, actual, options
+      require 'pact/matchers' # avoid recursive loop between this file and pact/matchers
+      ::Pact::Matchers.diff expected, actual, options
     end
-
-
   end
 end

lib/pact/shared/text_differ.rb
@@ -1,12 +1,10 @@
-require 'pact/matchers/difference'
 
 module Pact
   class TextDiffer
 
-    extend Matchers
-
     def self.call expected, actual, options = {}
-      diff expected, actual, options
+      require 'pact/matchers' # avoid recursive loop between this file and pact/matchers
+      ::Pact::Matchers.diff expected, actual, options
     end
 
   end