e5881e3db614e375ef97a6972137a1aa577b061b
Beth Skurrie <beth@bethesque.com>
2018-04-03 15:40:07 +1000

    chore: update commit message in release.sh

--- script/release.sh
+++ script/release.sh
@@ -5,5 +5,5 @@ git checkout -- lib/pact/support/version.rb
 bundle exec bump ${1:-minor} --no-commit
 bundle exec rake generate_changelog
 git add CHANGELOG.md lib/pact/support/version.rb
-git commit -m "Releasing version $(ruby -r ./lib/pact/support/version.rb -e "puts Pact::Support::VERSION")"
+git commit -m "chore(release): version $(ruby -r ./lib/pact/support/version.rb -e "puts Pact::Support::VERSION")"
 bundle exec rake release
