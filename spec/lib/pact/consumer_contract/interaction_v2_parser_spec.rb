804feca7472193395393e6746b9dc157094dfe98
YOU54F <yousafn@gmail.com>
2019-06-14 00:07:12 +0100

    test: add tests for query params

--- spec/lib/pact/consumer_contract/interaction_v2_parser_spec.rb
+++ spec/lib/pact/consumer_contract/interaction_v2_parser_spec.rb
@@ -49,6 +49,38 @@ module Pact
           expect(subject.provider_state).to eq "foo"
         end
       end
+
+      describe "no query params" do
+        it "doesnt add query params if they dont exist" do
+          expect(subject.request.query).to_not eq ''
+        end
+      end
+    end
+    describe ".call with query params" do
+      let(:interaction_hash) do
+        {
+          "description" => "description",
+          "request" => { "method" => "GET", "path" => "/" ,
+            "query" => 'param1=thing1&param2=thing2'},
+          "response" => { "status" => 200 },
+          "providerState" => "foo"
+        }
+      end
+
+      let(:options) do
+        {
+          pact_specification_version: Pact::SpecificationVersion.new("3.0")
+        }
+      end
+
+      subject { InteractionV2Parser.call(interaction_hash, options) }
+
+      describe "query params" do
+        it "encodes them as a hash" do
+          expect(subject.request.query).to include :param1 => ["thing1"]
+          expect(subject.request.query).to include :param2 => ["thing2"]
+        end
+      end
     end
   end
 end
