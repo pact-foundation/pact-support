02e260296fde4d6669a696c673a583e70fdddfa1
Beth Skurrie <beth@bethesque.com>
2020-11-25 11:11:17 +1100

    style: whitespace

--- spec/lib/pact/reification_spec.rb
+++ spec/lib/pact/reification_spec.rb
@@ -16,7 +16,6 @@ module Pact
     end
 
     describe "from term" do
-
       subject { Reification.from_term(response_spec) }
 
       it "converts regexes into real data" do
@@ -38,11 +37,9 @@ module Pact
       it "handles arrays" do
         expect(subject[:array]).to eql ['first', 'second']
       end
-
     end
 
     context "when reifying a Request" do
-
       let(:request){ Pact::Request::Expected.from_hash(method: 'get', path: '/', body: Pact::Term.new(generate: "sunny", matcher: /sun/))}
 
       subject { Reification.from_term(request) }
@@ -50,11 +47,9 @@ module Pact
       it "turns it into a hash before reifying it" do
         expect(subject[:body]).to eq("sunny")
       end
-
     end
 
     context "when SomethingLike" do
-
       let(:request) { Pact::SomethingLike.new({a: 'String'})}
 
       subject { Reification.from_term(request)}
@@ -62,11 +57,9 @@ module Pact
       it "returns the contents of the SomethingLike" do
         expect(subject).to eq({a: 'String'})
       end
-
     end
 
     context "when nested SomethingLike" do
-
       let(:request) {
         Pact::SomethingLike.new(
           {
@@ -83,11 +76,9 @@ module Pact
       it "returns the contents of the SomethingLike" do
         expect(subject).to eq({a: 'String', b: { c: 'NestedString' }})
       end
-
     end
 
     context "when ArrayLike" do
-
       let(:request) { Pact::ArrayLike.new({a: 'String'}, {min: 3})}
 
       subject { Reification.from_term(request)}
@@ -95,11 +86,9 @@ module Pact
       it "returns the contents of the ArrayLike" do
         expect(subject).to eq([{a: 'String'}, {a: 'String'}, {a: 'String'}])
       end
-
     end
 
     context "when Query" do
-
       let(:query) { QueryString.new(Pact::Term.new(generate: "param=thing", matcher: /param=.*/)) }
 
       subject { Reification.from_term(query)}
@@ -107,14 +96,12 @@ module Pact
       it "returns the contents of the generate" do
         expect(subject).to eq("param=thing")
       end
-
     end
 
     context "when Hash Query" do
+      let(:query) { QueryHash.new( {param: 'hello', extra: 'world'})}
 
       subject { Reification.from_term(query)}
-
-      let(:query) { QueryHash.new( {param: 'hello', extra: 'world'})}
 
       it "returns the hash in the natural order" do
         expect(subject).to eq("param=hello&extra=world")
@@ -132,9 +119,9 @@ module Pact
     end
 
     context "when Hash Query with embeded terms" do
-      subject { Reification.from_term(query)}
-
       let(:query) { QueryHash.new( {param: 'hello', extra: Pact::Term.new(generate: "wonderworld", matcher: /\w+world/)})}
+
+      subject { Reification.from_term(query)}
 
       it "returns the hash in the natural order, and fills in Terms appropriately" do
         expect(subject).to eq("param=hello&extra=wonderworld")
@@ -142,9 +129,9 @@ module Pact
     end
 
     context "when Hash Query with Arrays and multiple params with the same name" do
-      subject { Reification.from_term(query)}
-
       let(:query) { QueryHash.new( {param: 'hello', double: [Pact::Term.new(generate: "wonder", matcher: /\w+/), 'world'], simple: 'bye'})}
+
+      subject { Reification.from_term(query)}
 
       it "returns the hash in the natural order, and fills in Terms appropriately" do
         expect(subject).to eq("param=hello&double=wonder&double=world&simple=bye")
@@ -152,9 +139,9 @@ module Pact
     end
 
     context "when Hash Query with an ArrayLike" do
-      subject { Reification.from_term(query)}
-
       let(:query) { QueryHash.new(param: Pact.each_like("1", min: 2)) }
+
+      subject { Reification.from_term(query)}
 
       it "turns the hash into a string with the right number of params" do
         expect(subject).to eq("param=1&param=1")
