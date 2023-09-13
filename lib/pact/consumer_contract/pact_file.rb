6ecb99a6eb60b4186ec1020795f462c8517ed5f7
Gerard Molina <47140788+gmolki@users.noreply.github.com>
2023-09-13 05:55:56 +0200

    feat: handle-x509-certs

--- lib/pact/consumer_contract/pact_file.rb
+++ lib/pact/consumer_contract/pact_file.rb
@@ -109,6 +109,12 @@ module Pact
       http.ca_file = ENV['SSL_CERT_FILE'] if ENV['SSL_CERT_FILE'] && ENV['SSL_CERT_FILE'] != ''
       http.ca_path = ENV['SSL_CERT_DIR'] if ENV['SSL_CERT_DIR'] && ENV['SSL_CERT_DIR'] != ''
       http.set_debug_output(Pact::Http::AuthorizationHeaderRedactor.new(Pact.configuration.output_stream)) if verbose?(options)
+
+      if custom_x509_certificate?
+        http.cert = OpenSSL::X509::Certificate.new(x509_cert_file)
+        http.key = OpenSSL::PKey::RSA.new(x509_cert_key_file)
+      end
+
       if disable_ssl_verification?
         if verbose?(options)
           Pact.configuration.output_stream.puts("SSL verification is disabled")
@@ -148,6 +154,18 @@ module Pact
 
     def verbose?(options)
       options[:verbose] || ENV['VERBOSE'] == 'true'
+    end
+
+    def custom_x509_certificate?
+      ENV['X509_CERT_ENABLED'] == 'true'
+    end
+
+    def x509_cert_file
+      File.read(ENV['X509_CERT_FILE_PATH'])
+    end
+
+    def x509_cert_key_file
+      File.read(ENV['X509_CERT_KEY_FILE_PATH'])
     end
 
     def disable_ssl_verification?
