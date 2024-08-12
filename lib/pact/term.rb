d7707916b531ab872670b77597d1c2dde87d610f
Yousaf Nabi <yousaf@pactflow.io>
2024-08-12 16:38:30 +0100

    fix: use single quotes for pact term creation err msg
    fixes #46

--- lib/pact/term.rb
+++ lib/pact/term.rb
@@ -29,7 +29,7 @@ module Pact
       @matcher = attributes[:matcher]
       raise Pact::Error.new("Please specify a matcher for the Term") unless @matcher != nil
       raise Pact::Error.new("Please specify a value to generate for the Term") unless @generate != nil
-      raise Pact::Error.new("Value to generate \"#{@generate}\" does not match regular expression #{@matcher.inspect}") unless @generate =~ @matcher
+      raise Pact::Error.new("Value to generate '#{@generate}' does not match regular expression #{@matcher.inspect}") unless @generate =~ @matcher
     end
 
     def to_hash
